9d4cb66ecd4bd048fbaab5a16c9d453974b7ad89
Korobeyshchikov Daniil <danilkor2006@gmail.com>
2026-06-19 23:08:23 +0300

    problem1 solved

--- problem0-mmap-find-substrings/solution.c
+++ problem0-mmap-find-substrings/solution.c
@@ -6,11 +6,6 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-// Программе передаются два аргумента: имя файла и строка для поиска.
-// Необходимо найти все вхождения строки в текстовом файле,
-// используя отображение на память с помощью системного вызова mmap.
-// На стандартный поток вывода вывести список всех позиций (с нуля),
-// где встречается искомая строка, по одной на строку.
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -18,9 +13,54 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO: откройте файл, получите его размер через fstat,
-    //       отобразите в память через mmap,
-    //       найдите все вхождения argv[2] и выведите их позиции
+    const char *needle = argv[2];
+    size_t needle_len = strlen(needle);
+    if (needle_len == 0) {
+        return 0;
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+        perror("fstat");
+        close(fd);
+        return 1;
+    }
+
+    size_t file_size = (size_t)st.st_size;
+    if (file_size < needle_len) {
+        close(fd);
+        return 0;
+    }
+
+    char *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        return 1;
+    }
+
+    char first = needle[0];
+    char *cur = data;
+    char *end = data + file_size - needle_len + 1;
+    while (cur < end) {
+        cur = memchr(cur, first, (size_t)(end - cur));
+        if (cur == NULL) {
+            break;
+        }
+        if (memcmp(cur, needle, needle_len) == 0) {
+            printf("%zu\n", (size_t)(cur - data));
+        }
+        ++cur;
+    }
+
+    munmap(data, file_size);
+    close(fd);
 
     return 0;
 }
